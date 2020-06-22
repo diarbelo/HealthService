dc486b99cf79bd6417e3d9f10fb99c57471734b7
diegoabetancur <diego.betancur@cadena.com.co>
2020-06-21 23:47:23 -0500

    Implementing unit tests

---
+++ web-api-tests/BusinessLogic/BusinessLogicTests.cs
@@ -0,0 +1,32 @@
+﻿using Contracts;
+using HealthService;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace web_api_tests.BusinessLogicForAppointments
+{
+    public class BusinessLogicTests
+    {
+        private readonly Mock<IRepositoryWrapper> _mockRepo;
+        private readonly BusinessLogic _class;
+
+        public BusinessLogicTests()
+        {
+            _mockRepo = new Mock<IRepositoryWrapper>();
+            _class = new BusinessLogic(_mockRepo.Object);
+        }
+
+        [Fact]
+        public void Validate_CancelAppointment_ReturnsBool()
+        {
+            //Act
+            var result = _class.CanCancel(DateTime.Now);
+
+            //Assert
+            Assert.IsType<bool>(result);
+        }
+    }
+}
---
+++ web-api-tests/Controller/PatientControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Contracts;
+using HealthService.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace web_api_tests.Controller
+{
+    public class PatientControllerTests
+    {
+        private readonly Mock<IRepositoryWrapper> _mockRepo;
+        private readonly PatientController _controller;
+
+        public PatientControllerTests()
+        {
+            _mockRepo = new Mock<IRepositoryWrapper>();
+            _controller = new PatientController(_mockRepo.Object, null);
+        }
+
+        [Fact]
+        public void GetById_UnknownIdPassed_ReturnsNotFoundResult()
+        {
+            //Act
+            var notFoundResult = _controller.GetPatientById(00);
+
+            //Assert
+            Assert.IsType<ObjectResult>(notFoundResult.Result);
+        }
+
+        [Fact]
+        public void GetById_ExistingId_ReturnsOkResult()
+        {
+            //Act
+            var okResult = _controller.GetPatientById(71);
+
+            //Assert
+            Assert.IsType<ObjectResult>(okResult.Result);
+        }
+    }
+}
